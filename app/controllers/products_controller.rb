858d02ec0179f5ab156d0e995fcba173fe992300
Stoyan Nikolov <s.nikolov@clock-hs.com>
2023-11-27 20:29:13 +0200

    Updating views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,31 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
 
+  # GET /products or /products.json
   def index
     @products = Product.all
   end
 
+  # GET /products/1 or /products/1.json
   def show
   end
 
+  # GET /products/new
   def new
     @product = Product.new
   end
 
+  # GET /products/1/edit
   def edit
   end
 
+  # POST /products or /products.json
   def create
-    @product.assign_attributes(product_params)
+    @product = Product.new(product_params)
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to product_path(@product), notice: "Product was successfully created." }
+        format.html { redirect_to product_url(@product), notice: "Product was successfully created." }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -29,10 +34,11 @@ class ProductsController < ApplicationController
     end
   end
 
+  # PATCH/PUT /products/1 or /products/1.json
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to products_path(@product), notice: "Product was successfully updated." }
+        format.html { redirect_to product_url(@product), notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -41,8 +47,9 @@ class ProductsController < ApplicationController
     end
   end
 
+  # DELETE /products/1 or /products/1.json
   def destroy
-    @product.destroy
+    @product.destroy!
 
     respond_to do |format|
       format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
