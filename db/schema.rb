cb9bf2ce73d1c1412053373e8df5411d24df9f07
Stoyan Nikolov <s.nikolov@clock-hs.com>
2023-12-02 18:12:49 +0200

    Adding devise users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_23_151253) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_02_115541) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -82,6 +82,32 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_151253) do
     t.string "name", null: false
     t.string "address"
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.boolean "suspended", default: false
+    t.boolean "admin", default: false
+    t.boolean "manager", default: false
+    t.string "preferred_locale", default: "en", null: false
+    t.string "name"
+    t.string "country"
+    t.string "city"
+    t.string "address"
+    t.integer "zip_code"
+    t.string "gender"
+    t.date "birthday"
+    t.string "provider", limit: 50, default: "", null: false
+    t.string "uid", limit: 50, default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
